9e63251e5066580e58ae204f033a48425809c4ce
Daniel Castillo <dyland6@gmail.com>
2013-09-22 23:39:30 -0500

    carousell added

---
+++ public/static/js/plugins/agilecarousel/agile_carousel_data.php
@@ -0,0 +1,16 @@
+[{
+      "content": "<div class='slide_inner'><a class='photo_link' href='#'><img class='photo' src='http://placehold.it/330x330' alt='Bike'></a><a class='caption' href='#'>Sample Carousel Pic Goes Here And The Best Part is that...</a><h3>promo1</h3></div>",
+      "content_button": "<div class='thumb'><img src='http://placehold.it/60x40' alt='bike is nice'></div><p>Agile Carousel Place Holder</p>"
+}, {
+      "content": "<div class='slide_inner'><a class='photo_link' href='#'><img class='photo' src='http://placehold.it/330x330' alt='Paint'></a><a class='caption' href='#'>Sample Carousel Pic Goes Here And The Best Part is that...</a><h3>promo2</h3></div>",
+      "content_button": "<div class='thumb'><img src='http://placehold.it/60x40' alt='bike is nice'></div><p>Agile Carousel Place Holder</p>"
+}, {
+      "content": "<div class='slide_inner'><a class='photo_link' href='#'><img class='photo' src='http://placehold.it/330x230' alt='Tunnel'></a><a class='caption' href='#'>Sample Carousel Pic Goes Here And The Best Part is that...</a><h3>promo3</h3></div>",
+      "content_button": "<div class='thumb'><img src='http://placehold.it/60x40' alt='bike is nice'></div><p>Agile Carousel Place Holder</p>"
+}, {
+      "content": "<div class='slide_inner'><a class='photo_link' href='#'><img class='photo' src='http://placehold.it/330x230' alt='Bike'></a><a class='caption' href='#'>Sample Carousel Pic Goes Here And The Best Part is that...</a><h3>promo4</h3></div>",
+      "content_button": "<div class='thumb'><img src='http://placehold.it/60x40' alt='bike is nice'></div><p>Agile Carousel Place Holder</p>"
+}, {
+      "content": "<div class='slide_inner'><a class='photo_link' href='#'><img class='photo' src='http://placehold.it/330x230' alt='Paint'></a><a class='caption' href='#'>Sample Carousel Pic Goes Here And The Best Part is that...</a><h3>promo5</h3></div>",
+      "content_button": "<div class='thumb'><img src='http://placehold.it/60x40' alt='bike is nice'></div><p>Agile Carousel Place Holder</p>"
+}]
